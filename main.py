1ad20e870b4ee52f7957d04ece4edc2bb64dccbd
Serena <spuertas@uade.edu.ar>
2024-09-03 13:32:46 -0300

    Menu principal v1.5

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def choice_menu(): #Menu estandar, posibles modificaciones a futuro.
+"""
+Esto es un programa de gestion de una biblioteca.
+"""
+def choice_menu():
+    """
+    Opciones del menu.
+    """
     print("\nMenú Principal:")
     print("1. Ver libros por género")
     print("2. Ver libros por autor")
@@ -6,35 +12,38 @@ def choice_menu(): #Menu estandar, posibles modificaciones a futuro.
     print("4. Pedir prestado un libro")
     print("5. Devolver un libro")
     print("6. Dejar una review")
-    print("7. Salir")
+    print("7. Salir del programa")
     return None
 
 def main_menu():
-
+    """
+    Menu principal.
+    """
     while True:
 
         choice_menu()
         choice = input("...Seleccione una opción: ")
-
-        if choice == "1":
-            pass
-        elif choice == "2":
-            pass
-        elif choice == "3":
-            pass
-        elif choice == "4":
-            pass
-        elif choice == "5":
-            pass
-        elif choice == "6":
-            pass
-        elif choice == "7":
-            break
-        else: 
-            print("Opción no válida. Intente de nuevo.")
+        match choice:
+            case "1":
+                pass
+            case "2":
+                pass
+            case "3":
+                pass
+            case "4":
+                pass
+            case "5":
+                pass
+            case "6":
+                pass
+            case "7":
+                print("Saliendo...")
+                break
+            case _:
+                print("Opción no válida. Intente de nuevo.")
     return None
 
 
 if __name__ == "__main__":
     main_menu()
-
+    
