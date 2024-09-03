bcc15ee21a02f8c99b1afaf702cab0cd66afc155
Serena <spuertas@uade.edu.ar>
2024-09-03 14:53:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 """
 Esto es un programa de gestion de una biblioteca.
 """
+import json
+import csv
+
+
+                        #ESCRITURA Y LECTURA - JSON Y CSV
+def read_json(ruta):
+    """
+    Lee un archivo JSON y devuelve su contenido como un objeto de Python (lista o diccionario).
+    IMPORTANTE/PENTIENDE - Archivo JSON pendiente a escribir.
+    """
+    with open(ruta, "r", encoding="utf-8") as archivo:
+        return json.load(archivo)
+
+def read_csv(ruta):
+    """
+    lee un archivo CSV y devuelve su contenido como una lista de diccionarios.
+    IMPORTANTE/PENDIENTE - Revisar archivo CSV.
+    """
+    books = []
+    with open(ruta, "r", encoding="utf-8") as archivo:
+        reader = csv.DictReader(archivo)
+        for fila in reader:
+            books.append(fila)
+        return books
+
+
+                        #MENU PRINCIPAL
 def choice_menu():
     """
-    Opciones del menu.
+    Funcion que muestra las opciones para el usuario.
     """
     print("\nMenú Principal:")
     print("1. Ver libros por género")
@@ -17,8 +44,12 @@ def choice_menu():
 
 def main_menu():
     """
-    Menu principal.
+    Funcion del menu principal que controla el flujo del programa.
     """
+    book = read_csv("datos/archivos_csv/books.csv")
+    borrowed_book = read_json("datos/archivos_json/stock.json")
+    review = read_json("datos/archivos_json/review.json")
+
     while True:
 
         choice_menu()
@@ -46,4 +77,3 @@ def main_menu():
 
 if __name__ == "__main__":
     main_menu()
-    
