3f68e9fc39d6733c1f26a44e05eaf9b063f8ef3a
Serena <spuertas@uade.edu.ar>
2024-09-09 09:28:12 -0300

    Menu principal v2.5

--- Biblioteca/main.py
+++ Biblioteca/main.py
@@ -1,51 +1,96 @@
 """MENÚ PRINCIPAL"""
 
+import os
+from tabulate import tabulate
 
-def main_menu():
-    "Esta funcion controla el flujo del programa"
 
-    while True:
-        print("\nMENÚ PRINCIPAL")
-        for key in ["1", "2"]:
-            print(f"{key}: {main_choices[key]}")
-
-        choice = input("...Seleccione una opción: ")
-        match choice:
-
-            case "1":
-                sub_opciones = ["1.1", "1.2", "1.3", "1.4"]
-                for key in sub_opciones:
-                    print(f"{key}: {main_choices[key]}")
-
-                choice = input("...Seleccione una opción: ")
-                match choice:
-                    case "1.1":  # Busqueda de libros por titulo
-                        pass
-                    case "1.2":  # Busqueda de libros por autor
-                        pass
-                    case "1.3":  # Busqueda de libros por genero
-                        pass
-                    case "1.4":  # Volver al menu principal
-                        return
-
-            case "2":
-                pass
-
-            case _:
-                print("\n...Opción no válida, intente de nuevo.")
-                continue
-
+def clear_screen():
+    """Esta funcion limpia la pantalla a medida que el usuario navega entre las opciones."""
+    os.system("cls" if os.name == "nt" else "clear")
     return None
 
 
-main_choices = {
-    "1": "Buscar libro",
-    "1.1": "Buscar por titulo",
-    "1.2": "Buscar por autor",
-    "1.3": "Buscar por genero",
-    "1.4": "Volver al menu principal",
-    "2": "Devolver libro",
-}
+def mostrar_menu_principal():
+    """Muestra el menú principal."""
+    menu = [["1. Buscar libros", "2. Devolver Libros", "3. Salir del programa"]]
+    titulo = "MENÚ PRINCIPAL"
+    print(titulo.center(65, " "))
+    print("=" * 65)
+    print(
+        tabulate(menu, tablefmt="fancy_grid", colalign=("center", "center", "center"))
+    )
+    print("=" * 65)
+    return None
+
+
+def mostrar_submenu_buscar_libro():
+    """Muestra el submenú para buscar libros."""
+    submenu = [
+        [
+            "1. Buscar por tí1tulo",
+            "2. Buscar por autor",
+            "3. Buscar por género",
+            "4. Volver al menú principal",
+        ]
+    ]
+    titulo = "BUSCAR LIBRO"
+    print(titulo.center(100, " "))
+    print("=" * 100)
+    print(
+        tabulate(
+            submenu,
+            tablefmt="fancy_grid",
+            colalign=("center", "center", "center", "center"),
+        )
+    )
+    print("=" * 100)
+    return None
+
+
+def main_menu():
+    """Esta funcion controla el flujo del programa."""
+
+    while True:
+        clear_screen()
+        mostrar_menu_principal()
+        choice = input("\n...Seleccione una opción: ")
+        match choice:
+            case "1":  # BUSCAR LIBRO
+                while True:
+                    clear_screen()
+                    mostrar_submenu_buscar_libro()
+                    choice = input("\n...Seleccione una opción: ")
+                    match choice:
+                        case "1":  # Busqueda de libros por titulo
+                            pass
+                        case "2":  # Busqueda de libros por autor
+                            pass
+                        case "3":  # Busqueda de libros por genero
+                            pass
+                        case "4":  # Volver al menu principal
+                            break
+                        case _:
+                            clear_screen()
+                            input(
+                                "Opción no válida, intente de nuevo. Presione ENTER para continuar."
+                            )
+                            continue
+            case "2":  # DEVOLVER LIBRO
+                pass  # Validar input de documento de usuario
+
+            case "3":  # SALIR DEL PROGRAMA
+                clear_screen()
+                print("¡Adiós!")
+                break
+
+            case _:
+                clear_screen()
+                input(
+                    "Opción no válida, intente de nuevo. Presione ENTER para continuar."
+                )
+                continue
+    return None
+
 
 if __name__ == "__main__":
     main_menu()
