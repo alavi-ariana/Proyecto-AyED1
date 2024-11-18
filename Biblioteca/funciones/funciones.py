4da68cbfe48486c94f0e2e716a621af2f689d4b1
jjuanisima <jl9773788@gmail.com>
2024-11-18 17:56:13 -0300

    función leer_prestamos modificada

--- Biblioteca/funciones/funciones.py
+++ Biblioteca/funciones/funciones.py
@@ -26,29 +26,33 @@ def leer_libros() -> List:
 
     return datos
 
-def leer_prestamos() -> List:
-    """ Lectura del archivo json que contiene información sobre libros prestados.
-    Returns:
-        - List: Una lista de diccionarios donde cada diccionario representa un registro un libro
-                prestado. Cada diccionario contiene detalles relacionados con el libro prestado,
-                como el 'isbn-13' y 'quien lo retira'.
-    """
-    directorio = os.getcwd()
-    file_prestamos = os.path.join(
-        directorio, "Biblioteca", "JSON", "prestamos.json"
-        )
-    try:
-        with open (file_prestamos, 'r', encoding='UTF-8') as prestamos:
-            lector = json.load(prestamos)
-            datos = [linea for linea in lector]
-    except FileNotFoundError:
-        print(f'El archivo {file_prestamos} no se encontró.')
-        return []
-    except json.JSONDecodeError:
-        print(f'Error al decodificar el archivo JSON en {file_prestamos}.')
-        return []
+def leer_prestamos() -> dict:
+    '''
+    Lee el archivo de préstamos y devuelve los datos de los préstamos registrados
 
-    return datos
+    Pre:
+        No recibe ningún parámetro
+    Post:
+        Devuelve un diccionario con los datos de los préstamos, en caso contrario se muestra el error correspondiente o se crea el archivo
+    '''
+    directorio = os.getcwd()
+    file_prestamos = os.path.join(directorio, "Biblioteca", "JSON", "prestamos.json")
+    datos_leidos = {}
+    try:
+        with open(file_prestamos, 'r', encoding='UTF-8') as prestamos:
+            try:
+                datos_leidos = json.load(prestamos)
+            except json.JSONDecodeError:
+                print(f"Error al decodificar el archivo JSON en {file_prestamos}")
+                with open(file_prestamos, 'w', encoding='utf-8') as prestamos:
+                    json.dump(datos_leidos, prestamos, indent=4)
+    except FileNotFoundError:
+        with open(file_prestamos, 'w', encoding='utf-8') as prestamos:
+            json.dump(datos_leidos, prestamos, indent=4)
+        print(f"El archivo {file_prestamos} no existía. Se creó uno nuevo.")
+    except Exception as e:
+        print(f"Error desconocido al leer el archivo {file_prestamos}: {e}")
+    return datos_leidos
 
 """def leer_usuarios() -> List:
     Lectura del archivo donde se guardan los usuarios registrados.
